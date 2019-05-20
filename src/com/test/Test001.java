347a1954800a0860d2594679c9bdacf1ca1cecba
lisi <lisi@qq.com>
2019-05-20 16:01:19 +0800

    lq02

---
+++ src/com/test/Test001.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/**
+ * 时间:16:00
+ * 作者：Maibenben
+ **/
+public class Test001 {
+    public static void main(String[] args) {
+        System.out.println("ssssssssssssss");
+    }
+}
